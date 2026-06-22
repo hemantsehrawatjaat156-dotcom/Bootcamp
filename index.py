4252a3e99b7314f5ccc2cdd7dceaa221af37f612
hemant sehrawat <hemantsehrawatjaat156.com>
2026-06-22 11:57:54 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+class LibraryBook:
+    def __init__(self, title, author, book_id):
+        self.title = title
+        self.author = author
+        self.book_id = book_id
+
+    def display_info(self):
+        print("Book ID:", self.book_id)
+        print("Title:", self.title)
+        print("Author:", self.author)
+
+
+book1 = LibraryBook("Python Programming", "John Smith", 101)
+book2 = LibraryBook("Data Science with Python", "Jane Doe", 102)
+book3 = LibraryBook("Machine Learning Basics", "Alice Johnson", 103)
+book4 = LibraryBook("Deep Learning Fundamentals", "Bob Brown", 104)
+
+
+
+
+book1.display_info()
+book2.display_info()
+book3.display_info()
+book4.display_info()
