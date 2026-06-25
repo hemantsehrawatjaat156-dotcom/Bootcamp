b63e47d7fd179a735bc59b76344667a4d45ed81d
hemant sehrawat <hemantsehrawatjaat156.com>
2026-06-25 12:11:39 +0530

    ff

---
+++ test.py
@@ -0,0 +1,23 @@
+class animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+    
+    def speak(self):
+        return f"{self.name} says {self.sound}"
+    
+    def __str__(self):
+        return f"Animal({self.name})"
+    
+class dog(animal):
+    def __init__(self, name):
+        super().__init__(name, "Woof")
+        self.breed = breed
+
+    def fetch(self):
+        return f"{self.name} is fetching the ball!"
+    
+    d = Dog("Bruno", "Labrador")
+    print(d.speak())
+    print(d.fetch())
+    
