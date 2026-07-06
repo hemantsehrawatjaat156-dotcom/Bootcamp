43d26a054e797aefede40274b1be41d6c4bb0873
hemant sehrawat <hemantsehrawatjaat156.com>
2026-07-06 11:19:07 +0530

    hh

---
+++ num.py
@@ -0,0 +1,34 @@
+import numpy as np
+# arr1 = np.array([1, 2, 3])
+# arr2 = np.array([4, 5, 6])
+# sum_arr = arr1 + arr2
+# print("Sum of arrays:", sum_arr)
+# arr = np.array([1, 4, 9])
+# sprt_arr = np.sqrt(arr)
+# print("Square root of array:", sprt_arr)
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# reshaped_arr = arr.reshape(3, 2)
+# print("Reshaped array:\n", reshaped_arr)
+# arr1 = np.array([1, 2, 3])
+# arr2 = np.array([[4], [5], [6]])
+# broadcasted_sum = arr1 + arr2
+# print("Broadcasted sum of arrays:\n", broadcasted_sum)
+# arr = np.array([1, 2, 3, 4, 5])
+# bool_arr = arr > 3
+# filtered_arr = arr[bool_arr]
+# print("Filtered array:", filtered_arr)
+
+# arr = np.array([1, 2, 3, 4, 5])
+# percentile_50 = np.percentile(arr, 50)
+# print("50th Percentile:", percentile_50)
+# arr = np.array([1, 2, 3, 4, 5])
+# # cumsum_val = np.cumsum(arr)
+# # print("Cumulative sum of array:", cumsum_val)
+# cumprod_val = np.cumprod(arr)
+# print("Cumulative product of array:", cumprod_val)
+# arr1 = np.array([1, 2, 3])
+# arr2 = np.array([4, 5, 6])
+# correlation = np.corrcoef(arr1, arr2)
+# print("Correlation coefficient matrix:\n", correlation)
+# covariance = np.cov(arr1, arr2)
+# print("Covariance matrix:\n", covariance)
