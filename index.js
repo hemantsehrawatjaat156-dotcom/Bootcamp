f4b4a7b9a8d9d17e2fbc052b1e87982f79f555d4
hemant sehrawat <hemantsehrawatjaat156.com>
2026-07-16 10:28:00 +0530

    5

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log("Hello, World!");
+// let name = "Hemanth";
+// const birthyear = 2000;
+
+// console.log(`My name is ${name} and I was born in ${birthyear}.`);
+
+// let data ="JavaScript is Fun!"
+
+// let trimmedData = data.trim();
+// console.log("Trimmed Data:", trimmedData);
+
+// let extractedData = trimmedData.slice(0, 10);   
+// console.log("Extracted Data:", extractedData);
+/*
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log("Random Number:", randomNumber);
+*/
+
+// let checknumber = 7;
+
+// if (checknumber % 2 === 0) {
+//     console.log(`${checknumber} is an even number.`);
+// } else {
+//     console.log(`${checknumber} is an odd number.`);
+// // }
+// function greetUser(name) {
+//     console.log(`Hello, ${name}! Welcome to my web page.`)                                              ;
+// }
+
+// greetUser("Hemanth");
+// greetUser("John");
+// const calculaterectangleArea = (length, width) => {length * width};
+
+// let calculatedArea = calculaterectangleArea(5, 10);
+// console.log("Calculated Area:", calculatedArea);
+
+// function testScope() {
+//     var functionVar = "I am a function variable";
+
+//     if (true) {
+//         let blockVar = "I am a block variable";
+
+//         console.log("Inside block:");
+//         console.log(functionVar); 
+//         console.log(blockVar);   
+//     }
+
+//     console.log("Outside block but inside function:");
+//     console.log(functionVar); 
+   
+// }
+
+// testScope();
